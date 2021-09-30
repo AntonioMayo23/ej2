6c6cb94c73b3421c94765d936ca5575149f9c21c
AntonioMayo23 <91593603+AntonioMayo23@users.noreply.github.com>
2021-09-30 13:50:45 +0200

    Add files via upload

---
+++ ej2.py
@@ -0,0 +1,10 @@
+agua = 0
+agua = int(input("Dime la temperatura: "))
+if(agua < 100 and agua > 0):
+    print("El estado del agua es líquido")
+else: ()
+if(agua > 100):
+    print("El estado del agua es vapor")
+else: ()
+if(agua < 0):
+    print("El estado del agua es hielo")
